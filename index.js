4ba23e9247810227ba099e33545f9093c4079420
let-me-React <saurabh.gupta@nineleaps.com>
2021-01-12 15:12:19 +0530

    upper case and length

--- index.js
+++ index.js
@@ -6,4 +6,6 @@ const name = "saurabh";
 
 const string = `My name is ${name} and age is ${age}`;
 console.log(string);
+console.log(string.length); // find the length
+console.log(string.toUpperCase()); // to upper cAase
 //age = 3;
