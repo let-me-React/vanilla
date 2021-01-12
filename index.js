082dd0cfc7dbcc7fd5f40001cfe7877ed1501513
let-me-React <saurabh.gupta@nineleaps.com>
2021-01-12 15:20:32 +0530

    STRING TO ARRAY

--- index.js
+++ index.js
@@ -8,4 +8,6 @@ const string = `My name is ${name} and age is ${age}`;
 console.log(string);
 console.log(string.length); // find the length
 console.log(string.toUpperCase()); // to upper cAase
+const names = "Saurabh, Aanchal, Phalguni, Akansha";
+console.log(names.split(",")); // String to ARRAY
 //age = 3;
